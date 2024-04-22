185f8b1ab3d9302df0091631f76935baf9044155
suvojit1999 <164034159+suvojit1999@users.noreply.github.com>
2024-04-22 13:10:02 +0530

    Initial commit

---
+++ clear clutter/main.js
@@ -0,0 +1,23 @@
+import fs from "fs"
+import path from "path"
+
+// console.log(fs)
+const folderlocation = 'D:\\WEB DEV\\CODE WITH HARRY\\PRACTICE\\clear clutter'
+
+let AllFiles =  fs.readdirSync(folderlocation)
+// console.log(AllFiles)
+
+for (const i of AllFiles) {
+    const ext = i.split(".")[i.split(".").length - 1]
+    // console.log(ext)
+    if(ext !== 'js' && ext !== 'json' && i !== ext){
+        if (!fs.existsSync(ext)){
+            fs.mkdirSync(ext)
+            fs.renameSync(path.join(folderlocation, i), path.join(folderlocation , ext , i))
+        }
+        else {
+            fs.renameSync(path.join(folderlocation, i), path.join(folderlocation , ext , i))
+        }
+    }
+}
+
